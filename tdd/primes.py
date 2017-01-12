c4810810f611e6d2f87f0f28f48126a5e2a43efb
Scott Hajek <shajek@pivotal.io>
2017-01-12 16:03:01 -0500

    checkpoint up thru 25

--- tdd/primes.py
+++ tdd/primes.py
@@ -1,8 +1,14 @@
 def generate_prime_factors(n):
-    primes = [1]
-    if n % 2 == 0:
+    primes = []
+    while n % 2 == 0:
         primes.append(2)
         n /= 2
+    while n % 3 == 0:
+        primes.append(3)
+        n /= 3
+    while n % 5 == 0:
+        primes.append(5)
+        n /= 5
     if n > 1:
         primes.append(n)
     return tuple(primes)
--- test_tdd.py
+++ test_tdd.py
@@ -10,16 +10,41 @@ class TestPrimes(unittest.TestCase):
         pass
 
     def test_1(self):
-        self.assertEquals((1,), generate_prime_factors(1))
+        self.assertEquals((), generate_prime_factors(1))
 
     def test_2(self):
-        self.assertEquals((1,2), generate_prime_factors(2))
+        self.assertEquals((2,), generate_prime_factors(2))
 
     def test_3(self):
-        self.assertEquals((1, 3), generate_prime_factors(3))
+        self.assertEquals((3,), generate_prime_factors(3))
 
-    # def test_4(self):
-    #     self.assertEquals((1, 2), generate_prime_factors(4))
+    def test_4(self):
+        self.assertEquals((2, 2), generate_prime_factors(4))
+
+    def test_6(self):
+        self.assertEquals((2, 3), generate_prime_factors(6))
+
+    def test_7(self):
+        self.assertEquals((7,), generate_prime_factors(7))
+
+    def test_8(self):
+        self.assertEquals((2,2,2), generate_prime_factors(8))
+
+    def test_9(self):
+        self.assertEquals((3,3), generate_prime_factors(9))
+
+    def test_10(self):
+        self.assertEquals((2,5), generate_prime_factors(10))
+
+    def test_16(self):
+        self.assertEquals((2, 2, 2, 2), generate_prime_factors(16))
+
+    def test_25(self):
+        self.assertEquals((5, 5), generate_prime_factors(25))
+
+    def test_49(self):
+        self.assertEquals((7, 7), generate_prime_factors(49))
+
 
 if __name__ == '__main__':
     unittest.main()

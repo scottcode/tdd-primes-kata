d414695918444d2a237a24f38859346939355fe2
Scott Hajek <shajek@pivotal.io>
2017-01-12 15:20:32 -0500

    refactor

--- tdd/primes.py
+++ tdd/primes.py
@@ -1,5 +1,8 @@
 def generate_prime_factors(n):
+    primes = [1]
+    if n % 2 == 0:
+        primes.append(2)
+        n /= 2
     if n > 1:
-        return (1, n)
-    else:
-        return 1,
+        primes.append(n)
+    return tuple(primes)
